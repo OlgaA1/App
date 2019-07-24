db5e8c697a473a957bc5cca2768102f8ec08342e
3rkGalvan90 <erickgalvan90@gmail.com>
2019-07-24 09:09:24 -0500

    chest pt3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,33 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity_chest();
+            }
+        });
+    }
+    public void openActivity_chest(){
+        Intent intent = new Intent(this, chest.class);
+        startActivity(intent);
     }
 }
 
