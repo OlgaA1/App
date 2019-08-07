603a466df12ef7d06dcb7416a5d0085bad264c1b
3rkGalvan90 <erickgalvan90@gmail.com>
2019-08-07 10:14:55 -0500

    8-7-19

--- app/src/main/java/com/example/myapplication/arms.java
+++ app/src/main/java/com/example/myapplication/arms.java
@@ -2,13 +2,33 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class arms extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arms);
+
+        button = findViewById(R.id.finish);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityafter_workout();
+            }
+        });
+
+    }
+
+    public void openActivityafter_workout() {
+        Intent intent = new Intent(this, after_workout.class);
+        startActivity(intent);
     }
 }
+
